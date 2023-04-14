1277eff4acab315d866947c47afb7f72ef6257d0
zoya <zdzhnou@gmail.com>
2023-04-13 17:14:00 -0700

    more changes

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -9,7 +9,7 @@ void Robot::update_controller() {
 	m_controller_partner.print(1,1, "Intake Motor Temp: %f", m_intake.get_temp());
 }
 void Robot::update_drivetrain() {
-	m_drivetrain.update(m_controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X), m_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+	m_drivetrain.update(-1 * m_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y), m_controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X));
 	if (m_controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
 		m_drivetrain.next_reference_frame();
 	} else if (m_controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
@@ -58,11 +58,11 @@ void Robot::update_catapult() {
 	if (m_controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)) {
 		std::printf("X pressed");
 		// launches catapult forward
-		// m_catapult.spin_motor(0); 
-		m_catapult.spin_motor_no_limit(-11.8);
+		m_catapult.spin_motor(0); 
+		// m_catapult.spin_motor_no_limit(-11.8);
 		// moves it back to loading position
-		// m_catapult.spin_motor(1);
-		m_catapult.spin_motor_no_limit(-11.8);
+		m_catapult.spin_motor(1);
+		// m_catapult.spin_motor_no_limit(-11.8);
 	}
 	// presumably used to set it to load position
 	if (m_controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
